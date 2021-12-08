27a3bfae0e6855f5ff0ce90f8b947d654b2b726d
mkireva <mariya@kireva.de>
2021-12-08 17:10:43 +0100

    add scores.respository.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScoresModule } from './scores/scores.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import config from './conig/keys';
+import config from './database/config/keys';
 
 @Module({
   imports: [ScoresModule, MongooseModule.forRoot(config.mongoURI)],
